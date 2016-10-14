3d07c001decc29714e00dab879a281973b9315e6
lidox <dj.lido@web.de>
2016-10-14 11:35:47 +0200

    refactor

--- flink-processing-java/src/main/java/mailexample/Main.java
+++ flink-processing-java/src/main/java/mailexample/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	
 	
 	public static void main(String[] args) throws Exception {
-		String csvPath = "/home/lidox/Dokumente/bigdata-workspace/big-data-fun/flink-batch-processing-java/data/flinkMails.gz";
+		String csvPath = "/home/lidox/Dokumente/bigdata-workspace/big-data-fun/flink-processing-java/data/flinkMails.gz";
 		MailBatchProcessor batch = new MailBatchProcessor(csvPath);
 		batch.getUserAndMailCount();
 	}
