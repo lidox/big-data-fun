d2c8c6c0f1d3fd759a4192d31d33b259cd06578f
lidox <dj.lido@web.de>
2016-11-04 10:21:18 +0100

    add min and max metric for offline analysis

--- com.artursworld/src/main/java/reactiontest/offline/HumanBenchmark.java
+++ com.artursworld/src/main/java/reactiontest/offline/HumanBenchmark.java
@@ -47,7 +47,7 @@ public class HumanBenchmark {
 	 * Get reaction test count by sum function
 	 * @return the count of all reaction tests
 	 */
-	public int getReactionTestCount(){
+	public int getReactionTestCountBySum(){
 		int count = 0;
 		
 		try {
@@ -134,24 +134,70 @@ public class HumanBenchmark {
 			return retList;
 		}
 	}
+
+	/**
+	 * Get the reaction time with the minimal user count using the reduce function
+	 * @return the reaction time tuple with the minimal user count
+	 */
+	public Tuple2<Double, Integer> getReactionTimeByMinUserCount() {
+		Tuple2<Double, Integer> ret = new Tuple2<Double, Integer>();
+		try {
+			DataSet<Tuple2<Double, Integer>> minData = data.reduce(new ReduceFunction<Tuple2<Double,Integer>>() {
+			
+				private static final long serialVersionUID = 1439796543225086584L;
 	
-	/*
-	DataSet<Tuple2<Double, Integer>> data = inputData.map(new MapFunction<String, Tuple2<Double, Integer>>() {
-
-		private static final long serialVersionUID = -3098918429893723175L;
-
-		@Override
-		public Tuple2<Double, Integer> map(String value) throws Exception {
-			Double reactionTime = -1.;
-			Integer userCount = -1;
-			try {
-				//reactionTime = Double.parseDouble(value.f0);
-				//userCount = Integer.parseInt(value.f1);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			return new Tuple2<Double, Integer>(reactionTime, userCount);
+				@Override
+				public Tuple2<Double, Integer> reduce(Tuple2<Double, Integer> recent, Tuple2<Double, Integer> current) {
+					int recentUserCount = recent.f1;		
+					int currentUserCount = current.f1;
+					
+					if(recentUserCount < currentUserCount){
+						return recent;
+					} 
+					else{
+						return current;
+					}
+				}
+			});
+		
+			ret = minData.collect().get(0);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-	});
-	*/
+		return ret;
+	}
+
+	/**
+	 * Get the reaction time with the maximal user count using the reduce function
+	 * @return the reaction time tuple with the maximal user count
+	 */
+	public Tuple2<Double, Integer> getReactionTimeByMaxUserCount() {
+		Tuple2<Double, Integer> ret = new Tuple2<Double, Integer>();
+		try {
+			DataSet<Tuple2<Double, Integer>> maxData = data.reduce(new ReduceFunction<Tuple2<Double,Integer>>() { 
+			
+				private static final long serialVersionUID = 1439796543225086584L;
+	
+				@Override
+				public Tuple2<Double, Integer> reduce(Tuple2<Double, Integer> recent, Tuple2<Double, Integer> current) {
+					int recentUserCount = recent.f1;		
+					int currentUserCount = current.f1;
+					
+					if(recentUserCount > currentUserCount){
+						return recent;
+					} 
+					else{
+						return current;
+					}
+				}
+			});
+		
+			ret = maxData.collect().get(0);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return ret;
+	}
+	
+
 }
--- com.artursworld/src/main/java/reactiontest/offline/Main.java
+++ com.artursworld/src/main/java/reactiontest/offline/Main.java
@@ -1,5 +1,7 @@
 package reactiontest.offline;
 
+import org.apache.flink.api.java.tuple.Tuple2;
+
 /**
  * Offline data analysis
  *
@@ -8,9 +10,19 @@ public class Main {
 	
 	public static void main(String[] args) {
 		HumanBenchmark human = new HumanBenchmark();
-		//human.printInputData();
-		System.out.println("1. RT Count: "+human.getReactionTestCount()); // 1368464
+		human.printInputData();
+		
+		// metric: count
+		System.out.println("1. RT Count: "+human.getReactionTestCountBySum()); // 1368464
 		System.out.println("2. RT Count: "+human.getReactionTestCountByReduce()); // 1368464
+		
+		// metric: minimum
+		Tuple2<Double, Integer> recordWithMinUserCount = human.getReactionTimeByMinUserCount();
+		System.out.println("Min user count = "+recordWithMinUserCount.f1+" with RT = "+recordWithMinUserCount.f0);
+		
+		// metric maximum
+		Tuple2<Double, Integer> recordWithMaxUserCount = human.getReactionTimeByMaxUserCount();
+		System.out.println("Max user count = "+recordWithMaxUserCount.f1+" with RT = "+recordWithMaxUserCount.f0);
 	}
 	
 }
