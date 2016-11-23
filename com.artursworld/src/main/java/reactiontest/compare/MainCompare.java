4d6919ada7bc0308736dd2462b66135c490179b5
lidox <dj.lido@web.de>
2016-11-23 15:01:11 +0100

    fix #15 add some comments

--- com.artursworld/src/main/java/reactiontest/compare/MainCompare.java
+++ com.artursworld/src/main/java/reactiontest/compare/MainCompare.java
@@ -42,39 +42,49 @@ public class MainCompare {
 		// Prediction 2: Median off all reaction data since October 2016 
 		// and last reaction times specified by tumbling window
 		BatchFunctions batch = new BatchFunctions();
+		
+		// first get median of reaction times by offline data
 		DataSet<Tuple2<Double, Integer>> dataSet1 = batch.loadDataSetOfOctober2016();
 		double median = batch.getMedianReactionTime(dataSet1);
 		
-		
+		// second use calculated median to make a prediction
 		StreamFunctions stream = new StreamFunctions();
 		DataStream<Tuple7<String, String, Integer, String, Date, String, List<Double>>> dataStream1 = stream.getKafkaStream();
+		
+		// prediction for next reaction time = Median of the offlineAverage and onlineAverage using tumbling window
 		stream.printPredictionForNextReactionTimeByMedians(dataStream1, median, Time.seconds(10));
 		stream.execute();
 	}
 
 	private static void printPredictionByAverage() throws Exception {
 		// Prediction 1: Average off October 2016 reaction data + tumbling window
-		BatchFunctions human = new BatchFunctions();
-		DataSet<Tuple2<Double, Integer>> dataSet1 = human.loadDataSetOfOctober2016();
-		double average = human.getAverageReaction(dataSet1);
 		
+		// first get average reaction time by offline data
+		BatchFunctions batch = new BatchFunctions(); 
+		DataSet<Tuple2<Double, Integer>> dataSet1 = batch.loadDataSetOfOctober2016();
+		double average = batch.getAverageReaction(dataSet1);
 		
+		// second use calculated average to make a prediction
 		StreamFunctions stream = new StreamFunctions();
-		//START KAFKA Broker and Zookeeper
 		DataStream<Tuple7<String, String, Integer, String, Date, String, List<Double>>> dataStream1 = stream.getKafkaStream();
+		
+		// prediction for next reaction time = Mean of the offlineAverage and onlineAverage using tumbling window
 		stream.printPredictionForNextReactionTimeByAVGs(dataStream1, average, Time.seconds(10));
 		stream.execute();
 	}
 	
 	private static void printPredictionBySlidingAverage() throws Exception {
-		// Prediction 1: Average off October 2016 reaction data + tumbling window
+		// Prediction 3: Average off October 2016 reaction data + tumbling window
+		
+		// first get average reaction time by offline data
 		BatchFunctions human = new BatchFunctions();
 		DataSet<Tuple2<Double, Integer>> dataSet1 = human.loadDataSetOfOctober2016();
 		double average = human.getAverageReaction(dataSet1);
 		
-		
+		// second use calculated average to make a prediction
 		StreamFunctions stream = new StreamFunctions();
 		DataStream<Tuple7<String, String, Integer, String, Date, String, List<Double>>> dataStream1 = stream.getKafkaStream();
+		// prediction for next reaction time = Mean of the offlineAverage and onlineAverage using sliding window
 		stream.printPredictionForNextReactionTimeBySlidingAVGs(dataStream1, average, Time.seconds(10), Time.seconds(3));
 		stream.execute();
 	}
